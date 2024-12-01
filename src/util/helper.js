d44ab75da326a3de6967e281ba73fce69e517ce6
sami855-ux <samitale56@outlook.com>
2024-12-01 17:10:23 +0500

    added a function to formate the number

--- src/util/helper.js
+++ src/util/helper.js
@@ -15,4 +15,10 @@ function convertToFlag(countryCode) {
   return String.fromCodePoint(...codePoints)
 }
 
-export { weatherMap, convertToFlag }
+function formatNumber(num) {
+  if (num >= 1000) {
+    return (num / 1000).toFixed(1) + "k"
+  }
+  return num.toString()
+}
+export { weatherMap, convertToFlag, formatNumber }
